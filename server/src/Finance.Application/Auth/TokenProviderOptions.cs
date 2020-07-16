36c34d68ab746cbebf008beaecae3ecd4a1438b4
Stan Martens <stan-martens@hotmail.com>
2020-07-17 01:00:38 +0200

    Set up ASP.NET Identity for future JWT authentication
    Startup.cs configures ASP.NET Identity for the server. This is a bare setup of the framework with dbcontext and JWT configuration and migrations. The solution now also holds the Finance.Application and Finance.Infrastructure assemblies for separability.

---
+++ server/src/Finance.Application/Auth/TokenProviderOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Finance.Application.Auth
+{
+    public class TokenProviderOptions
+    {
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public SigningCredentials SigningCredentials { get; set; }
+        public TimeSpan ExpiresAt { get; set; }
+    }
+}
---
+++ server/src/Finance.Infrastructure/Identity/FinanceIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance.Infrastructure.Identity
+{
+    public class FinanceIdentityDbContext : IdentityDbContext<IdentityUser>
+    {
+        public FinanceIdentityDbContext(DbContextOptions<FinanceIdentityDbContext> options) : base(options)
+        {
+        }
+    }
+}
