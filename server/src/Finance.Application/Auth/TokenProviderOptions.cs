56a35fb2222c75438b4c4c454eca8158122c56ad
Stan Martens <stan-martens@hotmail.com>
2020-07-17 01:05:18 +0200

    Enable nullable reference types for all csprojs of the server

--- server/src/Finance.Application/Auth/TokenProviderOptions.cs
+++ server/src/Finance.Application/Auth/TokenProviderOptions.cs
@@ -5,9 +5,9 @@ namespace Finance.Application.Auth
 {
     public class TokenProviderOptions
     {
-        public string Audience { get; set; }
-        public string Issuer { get; set; }
-        public SigningCredentials SigningCredentials { get; set; }
+        public string? Audience { get; set; }
+        public string? Issuer { get; set; }
+        public SigningCredentials? SigningCredentials { get; set; }
         public TimeSpan ExpiresAt { get; set; }
     }
 }
