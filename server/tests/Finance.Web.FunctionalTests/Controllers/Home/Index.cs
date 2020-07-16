023a1ef3efe92b2dbf6de4e7b811169e2481ef01
Stan Martens <stan-martens@hotmail.com>
2020-07-16 19:28:26 +0200

    Add functional/unit tests for server and restructure dirs
    Homecontroller that redirects to Swagger docs now holds functional tests and unit tests. For organization, implementation and test code is not split up into src/ and tests/

---
+++ server/tests/Finance.Web.FunctionalTests/Controllers/Home/Index.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Finance.Web.FunctionalTests.Controllers.Home
+{
+    public class Index : IClassFixture<CustomWebApplicationFactoryFixture>
+    {
+        private readonly HttpClient _httpClient;
+
+        public Index(CustomWebApplicationFactoryFixture factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Should_respond_with_swagger_docs_on_redirect()
+        {
+            // Act
+            var swaggerPage = await _httpClient.GetAsync("/");
+            swaggerPage.EnsureSuccessStatusCode();
+
+            // Assert
+            swaggerPage.RequestMessage.RequestUri.Should().Be(_httpClient.BaseAddress + "swagger/index.html");
+        }
+    }
+}
---
+++ server/tests/Finance.Web.FunctionalTests/CustomWebApplicationFactoryFixture.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Finance.Web.FunctionalTests
+{
+    public class CustomWebApplicationFactoryFixture : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing");
+        }
+    }
+}
