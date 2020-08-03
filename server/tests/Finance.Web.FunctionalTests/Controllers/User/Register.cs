5c3fe83b8c4f477e9c720032c21d23322c81256e
smarten <user@example.com>
2020-08-03 22:23:48 +0200

    Implement functional test for registering user with happy flow

---
+++ server/tests/Finance.Web.FunctionalTests/Controllers/User/Register.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Finance.Web.Models.User;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Finance.Web.FunctionalTests.Controllers.User
+{
+    public class Register : IClassFixture<CustomWebApplicationFactoryFixture>
+    {
+        private readonly HttpClient _httpClient;
+
+        public Register(CustomWebApplicationFactoryFixture fixture)
+        {
+            _httpClient = fixture.CreateClient();
+        }
+
+        [Fact]
+        public async Task Should_respond_with_no_content_response_when_new_user_is_registered()
+        {
+            // Arrange
+            var viewModel = new RegisterViewModel
+            {
+                Email = "steve@gmail.com",
+                Password = "P@ssw0rd",
+                ConfirmPassword = "P@ssw0rd"
+            };
+            var jsonToPost = JsonConvert.SerializeObject(viewModel);
+
+            // Act
+            var response = await _httpClient.PostAsync("/users",
+                new StringContent(jsonToPost, Encoding.UTF8, MediaTypeNames.Application.Json));
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+    }
+}
--- server/tests/Finance.Web.FunctionalTests/CustomWebApplicationFactoryFixture.cs
+++ server/tests/Finance.Web.FunctionalTests/CustomWebApplicationFactoryFixture.cs
@@ -1,13 +1,69 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using System.Linq;
+using Finance.Infrastructure.Identity;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Finance.Web.FunctionalTests
 {
     public class CustomWebApplicationFactoryFixture : WebApplicationFactory<Startup>
     {
+        private bool _disposed;
+        private readonly SqliteConnection _sqliteConn;
+
+        public CustomWebApplicationFactoryFixture()
+        {
+            _disposed = false;
+            
+            _sqliteConn = new SqliteConnection("Data source=:memory:");
+            _sqliteConn.Open();
+        }
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseEnvironment("Testing");
+
+            builder.ConfigureServices(services =>
+            {
+                var dbContextOptionsDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<FinanceIdentityDbContext>));
+                services.Remove(dbContextOptionsDescriptor);
+
+                services.AddDbContext<FinanceIdentityDbContext>(options =>
+                {
+                    options.UseSqlite(_sqliteConn);
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var context = scopedServices.GetRequiredService<FinanceIdentityDbContext>();
+
+                    context.Database.EnsureCreated();
+                }
+            });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                _sqliteConn.Close();
+            }
+
+            _disposed = true;
+
+            base.Dispose(disposing);
         }
     }
 }
